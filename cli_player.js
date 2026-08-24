613f3e36aa46d7d6fd043a9b3afc20bdd4ab403f
Akshith17323 <akhi171356@gmail.com>
2026-08-24 11:29:52 +0530

    Interactive keyboard navigation for song selection in cli player

--- cli_player.js
+++ cli_player.js
@@ -1,39 +1,55 @@
 const { spawn } = require('child_process')
 const path = require('path')
-
 const { readdirSync } = require('fs')
 
-
+let songs = []
+let userSelectionIndex = 0
 const SONGS_DIR = "./songs"
 
-function listSongs(directory_path) {
-    // Input : Folder Path || Output: Content of Folder || ls
-    const scanner = spawn(
-        readdirSync(directory_path)
-    )
-    scanner.stdout.on('data', (data) => {
-        let songs = data.toString().trim().split("\n")
-        songs.forEach((song, ind) => {
-            console.log(`${ind} : ${song}`)
-        })
+function listSongs(songDirectoryPath) {
+    console.clear()
+    songs = readdirSync(songDirectoryPath).filter((file) => file.endsWith(".mp3"))
+    songs.forEach((song, ind) => {
+        if (ind === userSelectionIndex) {
+            console.log(`> ${song}`)
+        } else {
+            console.log(`  ${song}`)
+        }
     })
 }
+
 
 function playSong(song_name) {
     const song_path = path.join(SONGS_DIR, song_name)
     const player = spawn("afplay", [song_path])
+    console.log(`\nPlaying: ${song_name}`)
     return player
 }
 
 
 listSongs(SONGS_DIR);
 
-
-process.stdin.on('data', (data) => {
-    const userChoice = Number(data.toString())
-    console.log(`User chose ${userChoice}`)
-    console.log(songs)
-    console.log(SONGS_DIR + "/" + songs[userChoice])
-    playSong(SONGS_DIR + "/" + songs[userChoice])
-
+// Take User Song Selection
+process.stdin.setRawMode(true)
+process.stdin.on('data', (rawUserInput) => {
+    if (rawUserInput[0] === 0x0d) {
+        console.log(`\nUser selected: ${songs[userSelectionIndex]}`)
+        playSong(songs[userSelectionIndex])
+        return
+    }
+    if (rawUserInput[0] === 0x03) {
+        process.exit(0)
+    } else {
+        if (rawUserInput[0] === 0x1b) {
+            if (rawUserInput[1] === 0x5b) {
+                if (rawUserInput[2] === 0x41) { // Up Key
+                    userSelectionIndex = Math.max(0, userSelectionIndex - 1)
+                }
+                if (rawUserInput[2] === 0x42) { // Down Key
+                    userSelectionIndex = Math.min(songs.length - 1, userSelectionIndex + 1)
+                }
+            }
+        }
+    }
+    listSongs(SONGS_DIR)
 })
